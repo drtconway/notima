747cc046868d72086dc133ac31b12210e402464b
Thomas Conway <thomas.conway@petermac.org>
2021-01-12 10:10:50 +1100

    add binom bit widths

--- util/binom.py
+++ util/binom.py
@@ -11,11 +11,21 @@ def choose(n, k):
         return 1
     return fac(n) // (fac(k) * fac(n - k))
 
+def ind(i):
+    return ' ' * (4*i)
+
+def bit_width(x):
+    n = 0
+    while (x > 0):
+        n += 1
+        x >>= 1
+    return n
+
 N = 63
 
-print((' ' * 4) + 'static constexpr uint64_t* binom[] =')
-print((' ' * 8) + '{')
-for n in range(1, N+1):
+print(ind(2) + 'static constexpr uint64_t* binom[] =')
+print(ind(3) + '{')
+for n in range(0, N+1):
     row = []
     for k in range(0, n+1):
         row.append(choose(n,k))
@@ -23,7 +33,19 @@ for n in range(1, N+1):
     comma = ''
     if n < N:
         comma = ','
+    print(ind(4) + '(uint64_t[]){' + ', '.join(ss) + '}' + comma)
+print(ind(3) + '};')
+print()
+print(ind(2) + 'static constexpr uint8_t* binom_bits[] =')
+print(ind(3) + '{')
+for n in range(0, N+1):
+    row = []
+    for k in range(0, n+1):
+        row.append(bit_width(choose(n,k) - 1))
+    ss = [hex(x) + 'ull' for x in row]
+    comma = ''
+    if n < N:
+        comma = ','
+    print(ind(4) + '(uint8_t[]){' + ', '.join(ss) + '}' + comma)
+print(ind(3) + '};')
 
-    print((' ' * 12) + '(uint64_t[]){' + ', '.join(ss) + '}' + comma)
-
-print((' ' * 8) + '};')
